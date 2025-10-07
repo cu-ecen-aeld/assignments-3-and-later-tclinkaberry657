5c5f11fb524294b13102c5ddad56bbdc84e2f696
Tyler Clinkaberry <tylerclinkaberry@gmail.com>
2025-10-07 02:30:28 -0700

    Fixed finder.sh

--- finder-app/finder.sh
+++ finder-app/finder.sh
@@ -1,18 +1,16 @@
-#!/bin/bash
+#!/bin/sh
 
-if [ $# -ne 2 ]
-then
-    echo "Incorrect number of arguments specified.
+if [ $# -ne 2 ]; then
+  echo "Incorrect number of arguments specified.
     usage: finder.sh <filesdir> <searchstr>"
-    exit 1
+  exit 1
 fi
 
-if [ -d "$1" ]
-then
-    file_count=$(grep -rlI "$2" "$1" | wc -l)
-    line_count=$(grep -rI "$2" "$1" | wc -l)
-    echo "The number of files are ${file_count} and the number of matching lines are ${line_count}"
+if [ -d "$1" ]; then
+  file_count=$(grep -rlI "$2" "$1" | wc -l)
+  line_count=$(grep -rI "$2" "$1" | wc -l)
+  echo "The number of files are ${file_count} and the number of matching lines are ${line_count}"
 else
-    echo "<filesdir> not a valid directory."
-    exit 1
+  echo "<filesdir> not a valid directory."
+  exit 1
 fi
